d58414850166c43d9a5466457e73b122ff50d6d1
ptakondrej <florixakcz@seznam.cz>
2026-05-26 12:11:22 +0200

    fix: migration path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ config({ path: [".env.local", ".env"] });
 
 export default defineConfig({
   schema: "./db/schema.ts",
-  out: "./migrations",
+  out: "./db/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
