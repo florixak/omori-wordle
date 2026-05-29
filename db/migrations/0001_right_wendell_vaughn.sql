a304eb2b5634ea1e044d2a7d9502f8be49579c47
ptakondrej <florixakcz@seznam.cz>
2026-05-29 20:47:26 +0200

    fix: date regex checks and add DB scripts

--- db/migrations/0001_right_wendell_vaughn.sql
+++ db/migrations/0001_right_wendell_vaughn.sql
@@ -1,4 +1,4 @@
-ALTER TABLE "game_result" DROP CONSTRAINT "game_result_date_format_check";--> statement-breakpoint
-ALTER TABLE "user_stats" DROP CONSTRAINT "user_stats_last_played_date_format_check";--> statement-breakpoint
+ALTER TABLE "game_result" DROP CONSTRAINT IF EXISTS "game_result_date_format_check";--> statement-breakpoint
+ALTER TABLE "user_stats" DROP CONSTRAINT IF EXISTS "user_stats_last_played_date_format_check";--> statement-breakpoint
 ALTER TABLE "game_result" ADD CONSTRAINT "game_result_date_format_check" CHECK ("game_result"."date" ~ '^[0-9]{4}-[0-9]{2}-[0-9]{2}$');--> statement-breakpoint
-ALTER TABLE "user_stats" ADD CONSTRAINT "user_stats_last_played_date_format_check" CHECK ("user_stats"."last_played_date" ~ '^[0-9]{4}-[0-9]{2}-[0-9]{2}$');
+ALTER TABLE "user_stats" ADD CONSTRAINT "user_stats_last_played_date_format_check" CHECK ("user_stats"."last_played_date" IS NULL OR "user_stats"."last_played_date" ~ '^[0-9]{4}-[0-9]{2}-[0-9]{2}$');
