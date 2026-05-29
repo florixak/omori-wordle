52ff32fc10c967c9e88e7498654ec5d6da489514
ptakondrej <florixakcz@seznam.cz>
2026-05-29 09:03:15 +0200

    feat: create header with basic login

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,36 @@
-import Link from "next/link";
-import React from "react";
-import WordleTile from "./wordle-tile";
+"use client";
+
+import { BarChart, Link, LogOut, User } from "lucide-react";
+import WordleButton from "./wordle-button";
+import { authClient } from "@/lib/auth-client";
 
 const Header = () => {
+  const { data: session } = authClient.useSession();
+
+  const handleLogout = async () => {
+    await authClient.signOut();
+  };
+
+  const handleLogin = async () => {
+    await authClient.signIn.social({
+      provider: "discord",
+    });
+  };
+
   return (
-    <header className="flex justify-between items-center p-4">
-      <h1 className="text-2xl font-bold">OMORI Wordle</h1>
-      <nav>
-        <ul className="flex gap-4">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <WordleTile letter="O" display="active" />
-          </li>
-        </ul>
-      </nav>
+    <header className="absolute top-0 left-0 right-0 flex justify-end items-center p-4 gap-4">
+      {session ? <span>{session.user.name}</span> : null}
+      {session ? (
+        <WordleButton className="py-0" onClick={handleLogout}>
+          <LogOut size={24} />
+        </WordleButton>
+      ) : null}
+      <WordleButton className="py-0" onClick={handleLogin}>
+        <User size={24} />
+      </WordleButton>
+      <WordleButton className="py-0" onClick={handleLogout}>
+        <BarChart size={24} />
+      </WordleButton>
     </header>
   );
 };
