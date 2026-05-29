65fa79b82a6393e9802d6acda251839a49a1fa17
ptakondrej <florixakcz@seznam.cz>
2026-05-29 19:35:23 +0200

    fix: use [0-9] regex for date checks

---
+++ db/migrations/0001_right_wendell_vaughn.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "game_result" DROP CONSTRAINT "game_result_date_format_check";--> statement-breakpoint
+ALTER TABLE "user_stats" DROP CONSTRAINT "user_stats_last_played_date_format_check";--> statement-breakpoint
+ALTER TABLE "game_result" ADD CONSTRAINT "game_result_date_format_check" CHECK ("game_result"."date" ~ '^[0-9]{4}-[0-9]{2}-[0-9]{2}$');--> statement-breakpoint
+ALTER TABLE "user_stats" ADD CONSTRAINT "user_stats_last_played_date_format_check" CHECK ("user_stats"."last_played_date" ~ '^[0-9]{4}-[0-9]{2}-[0-9]{2}$');
