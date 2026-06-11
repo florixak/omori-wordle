c07f5568357ac2e362bddf644942b3cc41e05512
ptakondrej <florixakcz@seznam.cz>
2026-06-11 14:23:34 +0200

    feat: add confirm dialog

---
+++ components/dialog/confirm-dialog.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import {
+  OmoriDialog,
+  OmoriDialogContent,
+  OmoriDialogDescription,
+  OmoriDialogFooter,
+  OmoriDialogHeader,
+  OmoriDialogTitle,
+} from "../omori/omori-dialog";
+import WordleButton from "../wordle-button";
+
+type ConfirmDialogProps = {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  title: string;
+  description: string;
+  onConfirm: () => void | Promise<void>;
+  onCancel: () => void | Promise<void>;
+  isLoading: boolean;
+};
+
+const ConfirmDialog = ({
+  open,
+  onOpenChange,
+  title,
+  description,
+  onConfirm,
+  onCancel,
+  isLoading,
+}: ConfirmDialogProps) => {
+  return (
+    <OmoriDialog open={open} onOpenChange={onOpenChange}>
+      <OmoriDialogContent>
+        <OmoriDialogHeader>
+          <OmoriDialogTitle>{title}</OmoriDialogTitle>
+        </OmoriDialogHeader>
+        <OmoriDialogDescription>{description}</OmoriDialogDescription>
+        <OmoriDialogFooter>
+          <WordleButton
+            className="w-full gap-2"
+            onClick={onCancel}
+            tabIndex={0}
+            disabled={isLoading}
+          >
+            Cancel
+          </WordleButton>
+          <WordleButton
+            className="w-full gap-2"
+            onClick={onConfirm}
+            tabIndex={0}
+            disabled={isLoading}
+          >
+            Confirm
+          </WordleButton>
+        </OmoriDialogFooter>
+      </OmoriDialogContent>
+    </OmoriDialog>
+  );
+};
+
+export default ConfirmDialog;
