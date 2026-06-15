7a7b704b422ca1d5d59d1d5e1378456e01045fa1
ptakondrej <florixakcz@seznam.cz>
2026-06-15 12:56:13 +0200

    feat: add hanging light bulb image to title

--- components/wordle-title.tsx
+++ components/wordle-title.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type WordleTitleProps = {
   streak: number;
   isLoggedIn: boolean;
@@ -5,7 +7,18 @@ type WordleTitleProps = {
 
 const WordleTitle = ({ streak, isLoggedIn }: WordleTitleProps) => {
   return (
-    <div className="flex flex-col items-center gap-1 text-center">
+    <div className="flex flex-col items-center gap-1 text-center relative">
+      <div className="absolute left-1/2 -translate-x-1/2 -top-76 overflow-hidden">
+        <Image
+          src="/hanging-light-bulb.gif"
+          alt="Light bulb"
+          width={96}
+          height={288}
+          className="h-auto w-24"
+          priority
+        />
+      </div>
+
       <h1 className="font-pixel text-2xl sm:text-3xl md:text-4xl">
         Omori Wordle
       </h1>
